2df52eae5c202bb213ec33f47e7062e632e48d35
abhishekkp00 <abhishekforcollege@gmail.com>
2026-06-05 17:26:55 +0530

    Delete added

--- Backend/AbhiKharidoo/src/main/java/com/abhi/AbhiKharidoo/controllers/ProductController.java
+++ Backend/AbhiKharidoo/src/main/java/com/abhi/AbhiKharidoo/controllers/ProductController.java
@@ -51,7 +51,7 @@ public class ProductController {
     public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
         Product savedProduct = null;
         try {
-            savedProduct = productService.addProduct(product, imageFile);
+            savedProduct = productService.addOrUpdateProduct(product, imageFile);
             return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
         } catch (IOException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
@@ -63,10 +63,21 @@ public class ProductController {
     public ResponseEntity<String> updateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile){
         Product updatedProduct = null;
         try {
-            updatedProduct= productService.updateProduct(product, imageFile);
+            updatedProduct= productService.addOrUpdateProduct(product, imageFile);
             return new ResponseEntity<>("Updated", HttpStatus.OK);
         }catch (IOException e){
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable int id){
+        Product product = productService.getProductById(id);
+        if(product != null) {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>("Deleted", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
--- Backend/AbhiKharidoo/src/main/java/com/abhi/AbhiKharidoo/service/ProductService.java
+++ Backend/AbhiKharidoo/src/main/java/com/abhi/AbhiKharidoo/service/ProductService.java
@@ -24,7 +24,7 @@ public class ProductService {
         return productRepo.findById(id).orElse(new Product(-1));
     }
 
-    public Product addProduct(Product product, MultipartFile image) throws IOException {
+    public Product addOrUpdateProduct(Product product, MultipartFile image) throws IOException {
         product.setImageName(image.getOriginalFilename());
         product.setImageType(image.getContentType());
         product.setImageData(image.getBytes());
@@ -32,11 +32,7 @@ public class ProductService {
         return productRepo.save(product);
     }
 
-    public Product updateProduct(Product product, MultipartFile image) throws IOException {
-        product.setImageName(image.getOriginalFilename());
-        product.setImageType(image.getContentType());
-        product.setImageData(image.getBytes());
-
-        return productRepo.save(product);
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
     }
 }
